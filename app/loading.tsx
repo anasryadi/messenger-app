2ef2f778b14bff2ba92a6f6ddd75e063f6fb4ff2
anasryadi <anasryadi0@gmail.com>
2022-11-20 12:07:13 -0800

    added a sign in page on /auth/signin

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,11 +1,10 @@
-import React from "react";
-
 function Loading() {
   return (
-    <div className="text-center">
+    <div className="text-center pt-8">
+        <p className="text-blue-400 pb-5 animate-pulse">Loading Messenger</p>
       <div role="status">
         <svg
-          className="inline mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+          className="inline mr-2 w-16 h-16 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
           viewBox="0 0 100 101"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
